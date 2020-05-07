eba17db0c37b4cbab4b44fb469e5bd591d63b595
zhangmingming <zhangmingming@doweidu.com>
2020-05-07 17:39:16 +0800

    拓扑排序

---
+++ algorithm/topological_sorting.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * 拓扑排序: 由偏序集得到全序集
+ * @author: ZhangMingming
+ * Date: 2020/5/7
+ * Time: 下午4:39
+ */
+
+/**
+ * 寻找极小元
+ * @param $posetArr
+ * @return null|string
+ */
+function pop_minimal_element(&$posetArr)
+{
+    foreach ($posetArr as $large => &$smallArr) {
+        // 没有更小元
+        if (empty($smallArr)) {
+            unset($posetArr[$large]);
+            return $large;
+        }
+        foreach ($smallArr as $k => $small) {
+            if (!isset($posetArr[$small])) {
+                unset($smallArr[$k]);
+                return $small;
+            }
+        }
+    }
+    return null;
+}
+
+/**
+ * @param $posetArr
+ * @return array
+ */
+function topological_sort($posetArr)
+{
+    $totalOrderSet = [];
+    while ($me = pop_minimal_element($posetArr)) {
+        if (!in_array($me, $totalOrderSet)) {
+            $totalOrderSet[] = $me;
+        }
+    }
+    return $totalOrderSet;
+}
+
+$ch = fopen('php://stdin', 'r');
+if (!$ch) {
+    exit('failed to open input stream');
+}
+echo 'Read poset elements, one ordered pair each line. Blank line to end input.',PHP_EOL;
+echo 'Each ordered pair separated by comma, the large one at first, the small one at second.',PHP_EOL;
+$posets = [];
+while ($cnt = fgets($ch)) {
+    if (!($cnt = trim($cnt, " \n"))) {
+        break;
+    }
+    list($large, $small) = explode(',', $cnt);
+    if (!isset($posets[$large])) {
+        $posets[$large] = [];
+    }
+    $posets[$large][] = $small;
+}
+fclose($ch);
+
+foreach (topological_sort($posets) as $small) {
+    echo $small,PHP_EOL;
+}
