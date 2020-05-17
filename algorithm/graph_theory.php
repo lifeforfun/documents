a983a3900331547f8e712c2c63752eec64ce4184
zhangmingming <zhangmingming@doweidu.com>
2020-05-17 18:14:53 +0800

    图论

--- algorithm/graph_theory.php
+++ algorithm/graph_theory.php
@@ -7,9 +7,9 @@
  */
 
 /**
- * 构造有向图多重图邻接矩阵
+ * 构造有向多重图邻接矩阵
  * @param $vertexCnt int 顶点数
- * @param $edges array [ [v边->u边],...]
+ * @param $edges array [ [v边索引,u边索引],...]
  * @return array
  */
 function make_adjacent_matrix_directed_graph($vertexCnt,$edges)
@@ -24,7 +24,45 @@ function make_adjacent_matrix_directed_graph($vertexCnt,$edges)
     }
 
     foreach ($edges as $edge) {
-        $matrix[$edge[0]][$edge[1]] += 1;
+        if (count($edge)>1) {
+            list($v, $u) = $edge;
+            $matrix[$v][$u] += 1;
+        } else {
+            // 环
+            $matrix[$edge[0]][$edge[0]] += 1;
+        }
+    }
+
+    return $matrix;
+}
+
+/**
+ * 构造无向多重图关联矩阵
+ * @param $vertexCnt int 顶点数组
+ * @param $edges array [ [v边索引,u边索引],... ]
+ */
+function make_incidence_matrix_undirected_graph($vertexCnt, $edges)
+{
+    $matrix = [];
+    $edgeCnt = count($edges);
+
+    for($i=0;$i<$vertexCnt;$i++) {
+        $line = [];
+        for($j=0;$j<$edgeCnt;$j++) {
+            $line[] = 0;
+        }
+        $matrix[] = $line;
+    }
+
+    foreach ($edges as $idx => $edge) {
+        if (count($edge)>1) {
+            list($v, $u) = $edge;
+            $matrix[$v][$idx] += 1;
+            $matrix[$u][$idx] += 1;
+        } else {
+            // 环
+            $matrix[$edge[0]] += 1;
+        }
     }
 
     return $matrix;
