056b21a69e242e5b30a8c9f732a96b23bfd74d3f
zhangmingming <zhangmingming@doweidu.com>
2020-05-21 18:21:56 +0800

    图论:欧拉回路

--- algorithm/graph_theory.php
+++ algorithm/graph_theory.php
@@ -9,7 +9,7 @@
 /**
  * 构造有向多重图邻接矩阵
  * @param $vertexCnt int 顶点数
- * @param $edges array [ [v边索引,u边索引],...]
+ * @param $edges array [ [v顶点索引,u顶点索引],...]
  * @return array
  */
 function make_adjacent_matrix_directed_graph($vertexCnt,$edges)
@@ -28,8 +28,8 @@ function make_adjacent_matrix_directed_graph($vertexCnt,$edges)
             list($v, $u) = $edge;
             $matrix[$v][$u] += 1;
         } else {
-            // 环
-            $matrix[$edge[0]][$edge[0]] += 1;
+            // 环为边的度贡献2
+            $matrix[$edge[0]][$edge[0]] += 2;
         }
     }
 
@@ -39,7 +39,7 @@ function make_adjacent_matrix_directed_graph($vertexCnt,$edges)
 /**
  * 构造无向多重图关联矩阵
  * @param $vertexCnt int 顶点数组
- * @param $edges array [ [v边索引,u边索引],... ]
+ * @param $edges array [ [v顶点索引,u顶点索引],... ]
  */
 function make_incidence_matrix_undirected_graph($vertexCnt, $edges)
 {
@@ -60,10 +60,59 @@ function make_incidence_matrix_undirected_graph($vertexCnt, $edges)
             $matrix[$v][$idx] += 1;
             $matrix[$u][$idx] += 1;
         } else {
-            // 环
-            $matrix[$edge[0]] += 1;
+            // 环为边的度贡献2
+            $matrix[$edge[0]][$idx] += 2;
         }
     }
 
     return $matrix;
 }
+
+
+/**
+ * 基本法(套圈法)构造欧拉回路
+ */
+function euler_circuit($vertexCnt, $edges)
+{
+    $matrix = make_adjacent_matrix_directed_graph($vertexCnt, $edges);
+
+    print_matrix($matrix);
+}
+
+function print_matrix($matrix)
+{
+    $rowCnt = count($matrix);
+    $colCnt = count($matrix[0]);
+    $lines  = [];
+    for ($i=1;$i<=$rowCnt;++$i) {
+        // 添加表头行
+        if ($i===1) {
+            $lines[] = '';
+        }
+        $lines[] = '';
+
+        for($j=1;$j<=$colCnt;++$j) {
+            if ($i===1) {
+                if ($j===1) {
+                    $lines[0] = make_width(' ');
+                }
+                $lines[0] .= make_width('b'.$j);
+            }
+            if ($j===1) {
+                $lines[$i] .= make_width('a'.$i);
+            }
+            $lines[$i].= make_width($matrix[$i-1][$j-1]);
+        }
+    }
+    echo implode(PHP_EOL, $lines);
+}
+
+function make_width($text, $width=4)
+{
+    if (strlen($text)<$width) {
+        return str_pad($text, $width, ' ', STR_PAD_LEFT);
+    }
+    return $text;
+}
+
+euler_circuit(5, []);
